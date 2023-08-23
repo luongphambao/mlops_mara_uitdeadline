f59708f22c31b1884da40f4f4fca937e647afb61
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2023-08-23 20:24:22 +0700

    remove not use file

--- src/eda_submit.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-
-csv_path="data/data_phase1_prob1.csv"
-df=pd.read_csv(csv_path)
-#distribution of batch_id not sorted
-print(df["batch_id"].value_counts())
-print("check unique batch_id")
-print(len(df["batch_id"].unique()))
--- src/test.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from typing import Any, Dict, List, Optional
-class InferenceResponse(BaseModel):
-    id: Optional[str]
-    prediction: Optional[List[float]]
-    is_drift: Optional[int]
-response=InferenceResponse()
